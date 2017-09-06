138805b92b9c5e6f02f17a8310a80efddbb7fd86
Anirudh B S <anirudhbs96@gmail.com>
2017-09-06 22:21:42 +0530

    added define, begin

--- lisp-interpreter.js
+++ lisp-interpreter.js
@@ -7,14 +7,94 @@ let standardEnv = {
     '-' : input => input[0] - input[1],
     '*' : input => input.reduce((product, num) => product*num, 1),
     '/' : input => input[0] / input[1],
-    '=' : input => input[0] === input[1] ? true : false,
-    '<' : input => input[0] < input[1] ? true : false,
-    '>' : input => input[0] > input[1] ? true : false,
-    '<=' : input => input[0] <= input[1] ? true : false,
-    '>=' : input => input[0] >= input[1] ? true : false
+
+    '=' : input => input[0] === input[1],
+    '<' : input => input[0] < input[1],
+    '>' : input => input[0] > input[1],
+    '<=' : input => input[0] <= input[1],
+    '>=' : input => input[0] >= input[1],
+
+    'if' : input => input[0] ? input[1] : input[2],
+    'define' : input => standardEnv[input[0]] = input[1],
+    'begin' : input => input.slice(0, -1),
+    'write' : input => input[0],
+
+    'abs' : input => Math.abs(input[0]),
+    'max' : input => input.reduce((array, value) => Math.max(array, value)),
+    'min' : input => input.reduce((array, value) => Math.min(array, value)),
+
+    'lambda' : input => lambdaParser(input)
 }
-/*      'abs':     abs,
-        'append':  op.add,
+
+const spaceParser = function(input){
+    return /^(\s)+/.exec(input) ? input.replace(/^(\s)+/, '') : input
+}
+
+const numberParser = function(input){
+    let string = /^[-+]{0,1}\d+/.exec(input)
+    return string ? [parseFloat(string[0]), input.slice(string[0].length)] : null
+}
+
+const identifierParser = function(input){
+    let string = /^\w+/.exec(input)
+    return string ? [string[0], input.slice(string[0].length)] : null
+}
+
+const expressionParser = function(input){
+    if(input[0] !== '(') return null
+    input = input.slice(1)
+    let array = []
+    while(input[0] !== ')'){
+        input = spaceParser(input)
+        if(input[0] === ')') break
+        let string = valueParser(input)
+        if(!string){
+            let EoF = input.indexOf(' ')
+            array.push(input.slice(0, EoF))
+            input = input.slice(EoF)
+        }
+        else{
+            array.push(string[0])
+            input = string[1]
+        }
+        input = spaceParser(input)
+        if(input[0] === ')') break
+        let value = expressionParser(input)
+        if(value){
+            array.push(value[0])
+            input = value[1]
+        }
+    }
+    return [functionParser(array), input.slice(1)]
+}
+
+const valueParser = function(input){
+    let result = (expressionParser(input) || numberParser(input) || identifierParser(input))
+    return result
+    return null
+}
+
+const functionParser = function(input){
+    let operator = input[0], operation = null
+    input.shift()
+    let keys = Object.keys(standardEnv)
+    for(let ar_i in keys)
+        if(operator === keys[ar_i]){
+            operation = standardEnv[keys[ar_i]]
+            break
+        }
+    return operation(input)
+}
+
+const lambdaParser = function(input){
+    console.log(input);
+    return true
+}
+
+console.log(valueParser(file))
+
+// IDEA: lambda
+/*      'append':  op.add,
         'apply':   apply,
         'begin':   lambda *x: x[-1],
         'car':     lambda x: x[0],
@@ -26,8 +106,6 @@ let standardEnv = {
         'list':    lambda *x: list(x),
         'list?':   lambda x: isinstance(x,list),
         'map':     map,
-        'max':     max,
-        'min':     min,
         'not':     op.not_,
         'null?':   lambda x: x == [],
         'number?': lambda x: isinstance(x, Number),
@@ -35,81 +113,3 @@ let standardEnv = {
         'round':   round,
         'symbol?': lambda x: isinstance(x, Symbol),
 */
-const spaceParser = function(input){
-    return /^(\s)+/.exec(input) ? input.replace(/^(\s)+/, '') : input
-}
-
-/*
-function tokenize(input){ //convert string to tokens
-    input = input.replace(/\(/g, ' ( ').replace(/\)/g, ' ) ').trim().split(/\s+/g)
-}
-*/
-
-const numberParser = function(input){
-    let string = /^\d+/.exec(input)
-    return string ? [parseFloat(string[0]), input.slice(string[0].length)] : null
-}
-
-const identifierParser = function(input){
-    let string = /^\w/.exec(input)
-    return string ? [string[0], input.slice(string[0].length)] : null
-}
-
-const expressionParser = function(input){
-    if(input[0] !== '(') return null
-    input = input.slice(1)
-
-    let array = []
-    while(input[0] !== ')'){
-        input = spaceParser(input)
-        if(input[0] === ')') break
-
-        let string = valueParser(input)
-        if(!string){
-            let EoF = input.indexOf(' ')
-            let f = input.slice(0, EoF)
-            //console.log(f)
-            array.push(f)
-            input = input.slice(EoF)
-        }
-        else{
-            array.push(string[0])
-            input = string[1]
-        }
-        input = spaceParser(input)
-        if(input[0] === ')') break
-        let value =  expressionParser(input)
-        //console.log(value)
-        if(value){
-        array.push(value[0])
-        input = value[1]
-        }
-    }
-    let oc = functionParser(array)
-    //console.log(oc)
-    return [oc, input.slice(1)]
-}
-
-const valueParser = function(input){
-    let result = (expressionParser(input) || numberParser(input) || identifierParser(input))
-        return result
-    return null
-}
-
-const functionParser = function(input){
-    let operator = input[0], operation
-    // input = input.slice(1)
-    input.shift()
-    let keys = Object.keys(standardEnv)
-    for(let ar_i in keys)
-        if(operator === keys[ar_i]){
-            operation = standardEnv[keys[ar_i]]
-            break
-        }
-    return operation(input)
-}
-
-console.log(valueParser(file))
-// IDEA: lambda
-// IDEA: if
-// IDEA: define
