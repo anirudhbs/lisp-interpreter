077b28816ca8f4d4eb407bc2f1a45131de75fb4f
Anirudh B S <anirudhbs96@gmail.com>
2017-09-08 13:10:56 +0530

    lambda parser parses arguments

--- lisp-interpreter.js
+++ lisp-interpreter.js
@@ -6,7 +6,6 @@ let standardEnv = {
     '-' : input => input[0] - input[1],
     '*' : input => input.reduce((product, num) => product*num, 1),
     '/' : input => input[0] / input[1],
-
     '=' : input => input[0] === input[1],
     '<' : input => input[0] < input[1],
     '<=' : input => input[0] <= input[1],
@@ -16,14 +15,12 @@ let standardEnv = {
     'if' : input => input[0] ? input[1] : input[2],
     'define' : input => standardEnv[input[0]] = input[1],
     'begin' : input => input[input.length - 1],
-
     'abs' : input => Math.abs(input[0]),
     'max' : input => input.reduce((array, value) => Math.max(array, value)), //...array
     'min' : input => input.reduce((array, value) => Math.min(array, value)),
-
     'list' : input => input,
     'car' : input => input[0],
-    'cdr' : input => input.slice(1),
+    'cdr' : input => input.slice(1)
 }
 
 let functions = ['if', 'define', 'begin', 'abs', 'max', 'min', 'list', 'car', 'cdr']
@@ -56,12 +53,12 @@ const expressionParser = function(input){
     let array = [], value = null
     while(input[0] !== ')'){
         input = spaceParser(input)
-        let value
-        value = valueParser(input) || functionParser(input)
+        let value = valueParser(input)
         if(!value) return null
         array.push(value[0])
         input = value[1]
     }
+    console.log(array);
     return [functionEvaluator(array, standardEnv), input.slice(1)]
 }
 
@@ -85,7 +82,31 @@ const functionEvaluator = function(input, currentEnv){
 }
 
 const lambdaParser = function(input){
+    if(input.slice(0, 7) !== '(lambda') return null
+    input = input.slice(9) //remove (lambda (
+    let argumentArray = []
+    while(input[0] !== ')'){    //arguments
+        let argument = identifierParser(input)
+        argumentArray.push(argument[0])
+        input = argument[1]
+        input = spaceParser(input)
+    }
+    secondaryEnv['arguments'] = argumentArray
+
+    input = input.slice(2) //remove ) .
+    let bodyString = '', count = 0
+    do{ //body
+        let char = input.slice(0,1)
+        if(char === '(') count++
+        if(char === ')') count--
+        bodyString += char
+        input = input.slice(1)
+    }while(input[0] !== ')' && count !== 0)
+    bodyString += ')'
+    secondaryEnv['body'] = bodyString
+    console.log(secondaryEnv);
     return true
 }
 
-fs.readFile(file, 'utf-8', (error, input) => console.log(valueParser(input)))
+//fs.readFile(file, 'utf-8', (error, input) => console.log(valueParser(input)))
+fs.readFile(file, 'utf-8', (error, input) => console.log(lambdaParser(input)))
