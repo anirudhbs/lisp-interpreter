a69311811561c9d0ab70bd16818f93983690a950
Anirudh B S <anirudhbs96@gmail.com>
2017-09-16 16:54:52 +0530

    refactoring

--- lisp-interpreter.js
+++ lisp-interpreter.js
@@ -13,15 +13,17 @@ let standardEnv = {
   '>=': input => input[0] >= input[1],
 
   'if': input => input[0] ? input[1] : input[2],
-  'define': input => standardEnv[input[0]] = input[1],
+  'define': input => standardEnv[input[0]] = parseFloat(input[1]),
   'begin': input => input[input.length - 1],
+  'begin': input => input.slice(0, input.length - 1),
   'abs': input => Math.abs(input[0]),
   'max': input => input.reduce((array, value) => Math.max(array, value)), //...array
   'min': input => input.reduce((array, value) => Math.min(array, value)),
   'list': input => input,
   'car': input => input[0],
   'cdr': input => input.slice(1),
-  'print': input => input[0]
+  'print': input => console.log(input[0]),
+  'pow': input => Math.pow(input[0], input[1])
 }
 
 let secondaryEnv = new Object()
@@ -36,27 +38,24 @@ const numberParser = input => {
 }
 
 const stringParser = input => {
-  let EoS = input.indexOf(' ')
-  // IDEA: print strings"
-  return input[0] === '"' ? [input.slice(1, EoS), input.slice(EoS + 1)] : null
+  let EoS = input.indexOf(' ') // IDEA: print strings"
+  return input.startsWith('"') ? [input.slice(1, EoS), input.slice(EoS + 1)] : null
 }
 
 const functionParser = input => {
-  let argument, fxn = /^[a-z]+/.exec(input) || /^[/*-+]/.exec(input) || /^[=<>]{1,2}/.exec(input)
-  if (fxn) argument = spaceParser(input.slice(fxn[0].length))
-  return fxn ? [fxn[0], argument] : null
+  let fxn = /^[a-z]+/.exec(input) || /^[/*-+]/.exec(input) || /^[=<>]{1,2}/.exec(input)
+  if (fxn) input = spaceParser(input.slice(fxn[0].length))
+  return fxn ? [fxn[0], input] : null
 }
 
 const expressionParser = input => {
   if (input[0] !== '(') return null
-  input = spaceParser(input.slice(1))
+  input = input.slice(1)
   let array,
     value
-  if (array = lambdaParser(input) || defineParser(input)) return array
-  //if (array = lambdaParser(input) || defineParser(input) || macroParser(input)) return array
+  if (array = lambdaParser(input)) return array
   array = []
   while (input[0] !== ')') {
-    input = spaceParser(input)
     value = valueParser(input)
     if (!value) return null
     array.push(value[0])
@@ -66,7 +65,7 @@ const expressionParser = input => {
 }
 
 const valueParser = input => {
-  //input = spaceParser(input)
+  input = spaceParser(input)
   return numberParser(input) || stringParser(input) || functionParser(input) || expressionParser(input)
 }
 
@@ -85,6 +84,21 @@ const functionEvaluator = input => {
 }
 
 const lambdaParser = input => {
+  if (!input.startsWith('define')) return null
+  input = input.slice(7)
+  let identifier,
+    result
+  identifier = functionParser(input)
+  input = spaceParser(identifier[1])
+  result = LambdaHelper(input.slice(1))
+  if (!result) return null
+  secondaryEnv[identifier[0]] = result[0]
+  input = spaceParser(result[1])
+  input = input.slice(1)
+  return [null, input]
+}
+
+const LambdaHelper = input => {
   if (!input.startsWith('lambda')) return null
   input = input.slice(8)
   let argumentArray = new Array,
@@ -108,21 +122,6 @@ const lambdaParser = input => {
   return [fxn, input]
 }
 
-const defineParser = input => {
-  if (!input.startsWith('define')) return null
-  input = input.slice(7)
-  let identifier,
-    result
-  identifier = functionParser(input)
-  input = spaceParser(identifier[1])
-  result = lambdaParser(input.slice(1))
-  if (!result) return null
-  secondaryEnv[identifier[0]] = result[0]
-  result = spaceParser(result[1])
-  result = result.slice(1)
-  return [, result]
-}
-
 const procedure = (fxn, args) => {
   let keys = Object.keys(secondaryEnv)
   for (let ar_i in keys)
@@ -137,53 +136,13 @@ const procedure = (fxn, args) => {
   return expressionParser(body)[0]
 }
 
-/*(defmacro setTo10(num)
-(setq num 10)(print num))
-(setq x 25)
-(print x)
-(setTo10 x)*/
-
-const macroParser = input => {
-  input = input.slice(1) //remove (
-  if (!input.startsWith('defmacro')) return null
-  let argumentArray = new Array,
-    macro = {}
-  input = input.slice(9)
-  let EoS = input.indexOf('(') //name
-  macro.name = input.slice(0, EoS)
-  input = input.slice(EoS)
-  input = input.slice(1) // )
-
-  while (input[0] !== ')') { //arguments
-    input = spaceParser(input)
-    if (input[0] === ')') break
-    let value = valueParser(input)
-    argumentArray.push(value[0])
-    input = value[1]
-    input = spaceParser(input)
-  }
-  macro.argumentArray = argumentArray
-  input = input.slice(1)
-  input = spaceParser(input)
-
-  let EoM = input.indexOf('))') //body
-  let body = input.slice(0, EoM)
-  macro.body = input.slice(0, EoM + 1)
-  body.env = {}
-  input = input.slice(EoM + 1)
-  console.log(macro)
-  // IDEA:
-  return true
-}
-
 const parser = input => {
   while (input) {
     let temp = valueParser(input)
-    if (temp[0] !== undefined) console.log(temp[0])
+    if (temp[0]) console.log(temp[0])
     input = temp[1].slice(1)
     input = spaceParser(input)
   }
 }
 
-//fs.readFile(file, 'utf-8', (err, input) => parser(input))
-fs.readFile(file, 'utf-8', (err, input) => console.log(macroParser(input)))
+fs.readFile(file, 'utf-8', (err, input) => parser(input))
